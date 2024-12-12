b8af49b66e59d91008b8d366d284f55494790458
Arafat <kmyeaserarafat1971@gmail.com>
2024-12-12 13:49:26 +0600

    notice_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { RoomController } from './room/room.controller';
 import { RoomService } from './room/room.service';
 import { RoomModule } from './room/room.module';
+import { NoticeController } from './notice/notice.controller';
+import { NoticeService } from './notice/notice.service';
+import { NoticeModule } from './notice/notice.module';
 
 @Module({
-  imports: [RoomModule],
+  imports: [RoomModule, NoticeModule],
+  // controllers: [NoticeController],
+  // providers: [NoticeService],
   // controllers: [AppController, RoomController],
   // providers: [AppService, RoomService],
 })
